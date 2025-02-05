47e491b16c4e62991f59326884119c2ffa7e5433
Tokhtar <jura.tokhtar@gmail.com>
2025-02-05 14:17:55 +0200

    added grid card components

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Grid from './components/Grid/Grid';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Project Grid</h1>
+      </header>
+      <main>
+        <Grid />
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
